5b4c4789889948e87b022e91c52e99cd88a6686d
Andrew Fedina <veselchak@gmail.com>
2021-11-11 00:16:46 +0200

    Add script for display tooltip

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector("#myModal");
 const info = document.querySelector(".main__skill_block");
 const closeElement = document.querySelector("#close_btn");
+const tooltip = document.querySelector("#tooltip");
 
 info.addEventListener("click", () => {
 	modal.classList.add("show_modal");
@@ -10,4 +11,19 @@ info.addEventListener("click", () => {
 closeElement.addEventListener("click", () => {
 	modal.classList.remove("show_modal");
 	document.body.classList.remove("fixed");
+});
+
+info.addEventListener("mouseover", () => {
+	let cursorPositionX = window.event.pageX;
+	let cursorPositionY = window.event.pageY;
+	if (document.documentElement.clientWidth - cursorPositionX < 100) {
+		cursorPositionX -= 200;
+	}
+	tooltip.style.left = cursorPositionX + "px";
+	tooltip.style.top = cursorPositionY + "px";
+	tooltip.style.display = "block";
+});
+
+info.addEventListener("mouseout", () => {
+	tooltip.style.display = "none";
 });
